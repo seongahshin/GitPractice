6f7b2f111f55ee77852e38eb3f949744f4298419
seongahshin <cindy903c@naver.com>
2024-04-18 19:56:50 +0900

    Initial Commit

---
+++ GitPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitPractice
+//
+//  Created by 신승아 on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by 신승아 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
